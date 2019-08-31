158f05959d7190c993d3a2a76ce870b7c6367b0f
caenjuji <caenjuji@gmail.com>
2019-08-31 13:13:35 -0500

    Guardar citas en localStorage

--- src/App.js
+++ src/App.js
@@ -2,10 +2,25 @@ import React, { Component } from 'react'
 import './bootstrap.min.css'
 import Header from './components/Header'
 import NuevaCita from './components/NuevaCita'
+import ListaCitas from './components/ListaCitas'
 
 class App extends Component {
   state = {
     citas: []
+  }
+
+  //cuando la aplicación carga
+  componentDidMount() { 
+    const citasLS = localStorage.getItem('citas')
+    if (citasLS){
+      this.setState({
+        citas: JSON.parse(citasLS)
+      })
+    }
+  }
+  //cuando agregamos o eliminamos una nueva cita
+  componentDidUpdate(){
+    localStorage.setItem('citas', JSON.stringify(this.state.citas))
   }
 
   crearNuevaCita = datos => {
@@ -16,6 +31,21 @@ class App extends Component {
     this.setState({
         citas
     })
+  }
+
+  //eliminar las citas del state
+  eliminarCita = id => {
+    //tomar una copia del state
+    const citasActuales = [...this.state.citas]
+
+    //utilizar filter para sacar el elemento con el id 
+    const citas = citasActuales.filter(cita => cita.id !== id)
+
+    //actualizar el state
+    this.setState({
+      citas
+    })
+
   }
   render() {
     return (
@@ -29,7 +59,12 @@ class App extends Component {
                     crearNuevaCita={ this.crearNuevaCita }
                 />
               </div>
-              
+              <div className="mt-5 col-md-10 mx-auto">
+                <ListaCitas 
+                  citas={ this.state.citas }
+                  eliminarCita = { this.eliminarCita }
+                />
+              </div>
           </div>
       </div>
     );
--- src/components/NuevaCita.jsx
+++ src/components/NuevaCita.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import uuid from 'uuid'
+import PropTypes from 'prop-types'
 
 const stateInicial = {
     cita: {
@@ -135,6 +136,10 @@ class NuevaCita extends Component {
             </div>
         )
     }
+}
+
+NuevaCita.propTypes = {
+    crearNuevoCita: PropTypes.func.isRequired
 }
 
 export default NuevaCita
